ffd8d3c52f2453a71e214cac003ff4d6fb8f300d
Your Name <your.email@example.com>
2025-09-30 19:08:38 +0900

    Fix: ES 모듈에서 __dirname 사용을 위한 next.config.js 수정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-const path = require('path');
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -16,4 +20,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
