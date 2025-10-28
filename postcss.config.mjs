71e189e05919ef7de8cfe0299208ab02c459fe44
versuvic-ctrl <versuvic@gmail.com>
2025-10-28 17:04:26 +0900

    test

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,9 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
+// ✅ postcss.config.mjs
 const config = {
-  plugins: [tailwindcss, autoprefixer],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
 
 export default config;
