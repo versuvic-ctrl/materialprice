6a3964ebd898edeca8929d2e83609d3f4ebca565
versuvic-ctrl <versuvic@gmail.com>
2025-10-28 17:07:09 +0900

    test

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,9 @@
-export default {
+// ✅ Tailwind v4 이상용 설정
+const config = {
   plugins: {
-    tailwindcss: {},
+    "@tailwindcss/postcss": {},
     autoprefixer: {},
   },
 };
+
+export default config;
